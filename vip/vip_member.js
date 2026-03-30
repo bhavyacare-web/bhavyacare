1d6a0d539426244b44ee78ef272f5b909092011e
bhavyacare-web <bhavyacare1@gmail.com>
2026-03-30 17:30:43 +0530

    Update vip_member.js

--- vip/vip_member.js
+++ vip/vip_member.js
@@ -1,4 +1,4 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzFX5MRGW1XRhVVO1ABuKS8wi2lcN7PCcyNddHfzv407eZ6TyeWOesIf-FIbbTKu882vg/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw2osa0dczU6rUpl2vkEcMaok3WcsuUsqfWlqCdM5bxq_hzLK5LRbeLrCcR0X2qGuL9/exec";
 
 let baseVipPrice = 3000;
 let vipDiscount = 500;

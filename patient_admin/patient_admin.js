42bec8c820abb0bb16d6d1be64e2f31308827f97
bhavyacare-web <bhavyacare1@gmail.com>
2026-03-31 10:58:52 +0530

    Update patient_admin.js

--- patient_admin/patient_admin.js
+++ patient_admin/patient_admin.js
@@ -1,4 +1,4 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxpcc7ayrvTeT5WvWSgvYOapSGvX3TdHRYm8Ly55mbPmB9engVd9JoHNzQdqFDRcJWGzA/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyoFueFsKc2fZNCILbWFpErObvA37AIRzpfK76xSKvJMWdq2bW2ejoyFdXwcqAWW0DBGg/exec";
 let currentTab = 'patients';
 
 document.addEventListener("DOMContentLoaded", fetchPatientsData);

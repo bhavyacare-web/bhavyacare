a51753af8ac1f61dc0771468f6d1828e053eef6c
bhavyacare-web <bhavyacare1@gmail.com>
2026-04-16 12:46:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -302,13 +302,9 @@ function goToDashboard() {
             // Agar kabhi status undefined ho jaye toh default registration par
             window.location.href = "doctor/doctor_registration.html";
         }
-        // --- DASHBOARD ROUTING LOGIC --- (UPDATE KAREIN)
+    }
     else if (role === "pharmacy") {
         window.location.href = "pharmacy/pharmacy_registration.html"; // ✨ URL UPDATE
-    }
-    else if (role === "hospital" || role === "executive") {
-        alert("Redirecting to " + role.toUpperCase() + " Dashboard... (Under Construction)");
-    }
     }
     else if (role === "hospital" || role === "executive") {
         alert("Redirecting to " + role.toUpperCase() + " Dashboard... (Under Construction)");
